719c90a26987439b84196baed4ef7259aba5d4d7
mkhuda <mhmmd.huda@gmail.com>
2018-05-10 00:22:43 +0700

    create user_helper for sessions purpose

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -1,2 +1,33 @@
 module UserHelper
+	def log_in(user)
+		session[:user_id] = user.id
+	end
+
+	def log_out
+		forget(current_user)
+		session.delete(:user_id)
+		@current_user = nil
+	end
+
+	def forget(user)
+		user.forget
+		cookies.delete(:user_id)
+	end
+
+	def current_user
+		if (user_id = session[:user_id])
+			@current_user ||= User.find_by(id: session[:user_id])
+		elsif (user_id = cookies.signed[:user_id])
+			user = User.find_by(id: user_id)
+			if user
+				log_in user
+				@current_user = user
+			end
+		end
+	end
+
+	def logged_in?
+		!current_user.nil?
+	end
 end
+
