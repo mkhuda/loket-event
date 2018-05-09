101e3e3616ebc21d8062d47edf6d797c6d6740e3
mkhuda <mhmmd.huda@gmail.com>
2018-05-10 01:43:45 +0700

    add logout method routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,22 @@
 Rails.application.routes.draw do
+  
   root 'home#index'
+
   # twitter auth
   get 'twitter_auth', to: redirect('/auth/twitter'), as: 'twitter_auth'
   get '/auth/twitter/callback', to: 'user#twitter_auth'
   match "/auth/failure", to: 'user#failure',  via: [:get, :post]
+
+  # logout
+  delete 'logout', to: 'user#logout'
+
   # dashboard
   get 'dashboard', to: 'events#index'
+
   # resources
   resources :events do
     resources :tickets
   end
   resources :locations
+
 end
