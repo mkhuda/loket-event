7b8a73fec9c1b15b6f97663edc2aef39bda00ff0
mkhuda <mhmmd.huda@gmail.com>
2018-05-10 00:46:42 +0700

    store token on user model?

---
+++ db/migrate/20180509174309_add_token_store_to_user.rb
@@ -0,0 +1,6 @@
+class AddTokenStoreToUser < ActiveRecord::Migration[5.2]
+  def change
+    add_column :users, :twitter_token, :string
+    add_column :users, :twitter_secret, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_08_121223) do
+ActiveRecord::Schema.define(version: 2018_05_09_174309) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,6 +59,8 @@ ActiveRecord::Schema.define(version: 2018_05_08_121223) do
     t.string "twitter_username"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "twitter_token"
+    t.string "twitter_secret"
   end
 
   add_foreign_key "event_locations", "events"
