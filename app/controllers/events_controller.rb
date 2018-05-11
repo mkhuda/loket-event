f111f8b8b0cc952b16b0c75359ab3c105cb755b3
mkhuda <mhmmd.huda@gmail.com>
2018-05-12 03:16:13 +0700

    implement update for event created

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -31,6 +31,25 @@ class EventsController < ApplicationController
     @event_tickets = @event.tickets
   end
 
+  def edit
+    @event = current_user.events.find(params[:id])
+  end
+
+  def update
+    @event_params = event_params
+    @start_date = @event_params.select {|k, _| k.include? "event_start_date"}.values.join('-').to_date
+    @event = current_user.events.find(params[:id]).update_attributes(
+      name: @event_params[:name],
+      event_detail: @event_params[:event_detail],
+      event_start_date: @start_date,
+    )
+		if @event
+      redirect_to event_path(params[:id]), :flash => { :success => "Your event has been edited" }
+		else
+			render 'new'
+		end
+  end
+
 	private
 		def event_params
 			params.require(:event).permit(:name, :event_detail, :event_start_date, :event_end_date)
