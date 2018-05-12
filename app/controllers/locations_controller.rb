b514b8cce691074bde22f6489038b3129729fbf3
mkhuda <mhmmd.huda@gmail.com>
2018-05-12 16:59:50 +0700

    edit location functionality

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -21,9 +21,36 @@ class LocationsController < ApplicationController
     end
   end
 
+  def edit
+    @location = Location.find(params[:id])
+    @default_location = current_user.events.find(params[:event_id]).event_location
+  end
+
+  def update
+    @default_location = current_user.events.find(params[:event_id])
+    @lp = update_location_params
+    @create_master_location = Location.create(name: @lp[:name])
+    if @create_master_location
+      @create_event_location = current_user.events.find(params[:event_id])
+        .event_location.update_attributes(
+        location_id: @create_master_location.id,
+        description: @lp[:description]
+      )
+      redirect_to event_path(params[:event_id]),
+        :flash => { :success => "Your Location Has Been Set" }
+    else
+      render 'edit'
+    end
+
+  end
+
   private
     def location_params
       params.require(:location).permit(:name, :description)
+    end
+
+    def update_location_params
+      params.require(:event_location).permit(:name, :description)
     end
 
 	def require_authorization
