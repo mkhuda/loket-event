ab6fb600e0d5a3d21fd0b89320a82a7b45f975bb
mkhuda <mhmmd.huda@gmail.com>
2018-05-13 21:10:47 +0700

    add dashboard redirect assert response test

--- test/controllers/dashboard_controller_test.rb
+++ test/controllers/dashboard_controller_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class DashboardControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "get dashboard should be redirected without login" do
+    get dashboard_url
+    assert_response :redirect
+  end
 end
