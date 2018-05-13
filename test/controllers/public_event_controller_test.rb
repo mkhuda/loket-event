ade2eea5fb1beb6dc130ad22284079c637c71e7b
mkhuda <mhmmd.huda@gmail.com>
2018-05-13 20:50:18 +0700

    assert test of public event index

--- test/controllers/public_event_controller_test.rb
+++ test/controllers/public_event_controller_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class PublicEventControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should get index of public event" do
+    get public_event_url
+    assert_response :success
+  end
 end
